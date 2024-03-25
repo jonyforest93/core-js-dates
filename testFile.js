8500805832d78616926127e6bfab8329339954a8
jonyforest93 <jony.forest93@gmail.com>
2024-03-25 16:17:54 +0300

    feat: implemet isLeapYear

--- testFile.js
+++ testFile.js
@@ -1,15 +1,24 @@
-function getQuarter(date) {
-  const month = date.getUTCMonth();
+function getWorkSchedule(period, countWorkDays, countOffDays) {
+  const startDayParts = period.start.split('-');
+  const endDayParts = period.end.split('-');
+  const startDay = new Date(
+    Date.UTC(startDayParts[2], startDayParts[1] - 1, startDayParts[0])
+  );
+  const endDay = new Date(
+    Date.UTC(endDayParts[2], endDayParts[1] - 1, endDayParts[0])
+  );
 
-  if (month >= 0 && month <= 2) {
-    return 1;
+  let currentDay = startDay;
+  const schedule = [];
+  let work = 0;
+  let dayOff = 0;
+
+  while (currentDay.getTime() <= endDay.getTime()) {
+    if (work >= countWorkDays) {
+      work = 0;
+      currentDay.setDate(currentDay.getDate() + 1);
+      continue;
+    }
   }
-  if (month >= 3 && month <= 5) {
-    return 2;
-  }
-  if (month >= 6 && month <= 8) {
-    return 3;
-  }
-  return 4;
 }
-console.log(getQuarter(new Date(2024, 10, 10)));
+getWorkSchedule({ start: '01-01-2024', end: '15-01-2024' }, 1, 3);
